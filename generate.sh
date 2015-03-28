2b30fab662fd1f1a6b035883186bc665eb6c1308
Soumith Chintala <soumith@fb.com>
2015-03-28 13:26:00 -0400

    native clean

--- generate.sh
+++
@@ -1,5 +0,0 @@
-# call this, then remove the lapack calls for the non-float types, and the THVector* functions
-ffi-generate -f ~/torch-distro/install/include/TH/TH.h -l libTH -L /Library/Developer/CommandLineTools/usr/lib/ -m TH -p TH -s --I ~/torch-distro/install/include/TH/ >TH.js
-
-# then add ~/torch-distro/install/lib to DYLD_LIBRARY_PATH
-
